9ba959710613b74bb5971ab80b3d5b905b684dd5
letuwwa <letuwwa@gmail.com>
2021-05-20 23:28:59 +0300

    commit #3 refactoring

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,29 +5,27 @@ User = get_user_model()
 
 
 class EmployeeRegistrationForm(forms.ModelForm):
-    username = forms.CharField(max_length=10)
-    first_name = forms.CharField(label="First Name")
-    last_name = forms.CharField(label="Last Name")
-    email = forms.EmailField(label='Email address')
-    password = forms.CharField(widget=forms.PasswordInput)
-    confirm_password = forms.CharField(widget=forms.PasswordInput)
+    username = forms.CharField(max_length=10, label="Имя пользователя")
+    first_name = forms.CharField(label="Имя")
+    last_name = forms.CharField(label="Фамилия")
+    email = forms.EmailField(label='Почта')
+    password = forms.CharField(widget=forms.PasswordInput, label="Пароль")
+    confirm_password = forms.CharField(widget=forms.PasswordInput, label="Повторите пароль")
 
     class Meta:
         model = User
         fields = [
-
             'first_name',
             'last_name',
             'username',
             'email',
             'password',
             'confirm_password',
-
         ]
 
     def __init__(self, *args, **kwargs):
         super(EmployeeRegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['email'].widget.attrs['placeholder'] = 'Enter a valid e-mail'
+        self.fields['email'].widget.attrs['placeholder'] = 'Введите корректный e-mail'
 
 
 class EmployeeLoginForm(forms.Form):
@@ -36,7 +34,8 @@ class EmployeeLoginForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         super(EmployeeLoginForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget.attrs['placeholder'] = 'Username'
+        self.fields['username'].widget.attrs['placeholder'] = 'Имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Пароль'
 
     def clean(self, *args, **kwargs):
         username = self.cleaned_data.get('username')
@@ -45,9 +44,9 @@ class EmployeeLoginForm(forms.Form):
         if username and password:
             user = authenticate(username=username, password=password)
             if not user:
-                raise forms.ValidationError('This user does not exist')
+                raise forms.ValidationError('Такого пользователя не существует')
             if not user.check_password(password):
-                raise forms.ValidationError('Incorrect password')
+                raise forms.ValidationError('Некорректный пароль')
             if not user.is_active:
-                raise forms.ValidationError('This user is not active')
+                raise forms.ValidationError('Пользователь не активирован')
         return super(EmployeeLoginForm, self).clean(*args, **kwargs)
